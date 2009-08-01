792c348032a3da4acd8ca7dec563d55f5a2a21f7
Alvin Ye 叶冬开 <alvin.ye.cn@gmail.com>
2009-08-01 17:38:52 +0800

    impl config columns

--- lib/dry_view.rb
+++ lib/dry_view.rb
@@ -4,7 +4,8 @@ module DryView
   end
 
   module ClassMethods
-    def dry_view
+    def dry_view(options = {})
+      before_filter :set_config
       define_method("index") do
         index! do |format|
           format.html { render :template => "/shared/index" if !template_exists?  }
@@ -38,6 +39,15 @@ module DryView
       define_method("create") do
         create! do |success, failure|
           failure.html { render :template => template_exists? ? "new" : "/shared/new" }
+        end
+      end
+
+      define_method("set_config") do
+        @dry_view = {}
+        if options[:columns]
+          @dry_view[:columns] = resource_class.columns.select { |i| !options[:columns].select{|c| c.to_s == i.name}.empty? } 
+        else 
+          @dry_view[:columns] = resource_class.columns.reject { |i| ["id", "created_at", "updated_at"].include?(i.name) }
         end
       end
     end
--- lib/dry_view_helper.rb
+++ lib/dry_view_helper.rb
@@ -1,13 +1,2 @@
 module DryViewHelper
-  def list_columns
-    @list_columns ||= resource_class.columns.reject { |i| ["id", "created_at", "updated_at"].include?(i.name) }
-  end
-
-  def form_columns
-    @form_columns ||= resource_class.columns.reject { |i| ["id", "created_at", "updated_at"].include?(i.name) }
-  end
-
-  def show_columns
-    @show_columns ||= resource_class.columns.reject { |i| ["id", "created_at", "updated_at"].include?(i.name) }
-  end
 end   
