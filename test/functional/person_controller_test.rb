403a00d99707096c985af4343ce2d71398e7f711
Alvin Ye 叶冬开 <alvin.ye.cn@gmail.com>
2009-08-17 09:42:17 +0800

    add test view link

--- test/functional/person_controller_test.rb
+++ test/functional/person_controller_test.rb
@@ -5,12 +5,14 @@ class PersonControllerTest < ActionController::TestCase
     @controller  = PersonController.new
     @request     = ActionController::TestRequest.new
     @response    = ActionController::TestResponse.new
+
+    @alvin = Person.create!(:name => "alvin", :email => "alvin.ye.cn@gmail.com", :age => "27", :birthday => "1983-01-01")
   end
 
   test "index" do
     get :index
     assert_response :success
     assert_tag :tag => "h1", :attributes => {:class => "heading"}
+    assert_tag :tag => "a", :content => "View"
   end
-
 end
