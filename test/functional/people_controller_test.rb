1b7148bbc2526ecce5d8cceb46e32caa427023d3
JimJin <jim.jin2006@gmail.com>
2009-09-10 14:34:17 +0800

    refactory test.

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -6,32 +6,25 @@ class PeopleControllerTest < ActionController::TestCase
     @request     = ActionController::TestRequest.new
     @response    = ActionController::TestResponse.new
 
-    @no_permission = Person.create!(:name => "alvin", :email => "alvin.ye.cn@gmail.com", :age => "27", :birthday => "1983-01-01")
+    @allow_person = Person.create!(:name => "alvin", :email => "alvin.ye.cn@gmail.com", :age => "27", :birthday => "1983-01-01")
+    @not_allow_person = Person.create!(:name => "alvin", :email => "alvin.ye.cn@gmail.com", :age => "200", :birthday => "1983-01-01")
   end
 
   test "index" do
     get :index
     assert_response :success
-    assert_tag :tag => "h1", :attributes => {:class => "heading"}
-  end
+    assert_human_name
 
-  test 'have record actions' do
-    get :index
-    assert_tag :tag => "a", :content => "View"
-    assert_tag :tag => "a", :content => "Edit"
-    assert_tag :tag => "a", :content => "Remove"
-  end
+    assert_tag :tag => "a", :content => "View", :attributes => {:href => /\/#{@allow_person.id}/}
+    assert_tag :tag => "a", :content => "Edit", :attributes => {:href => /\/#{@allow_person.id}\/edit/}
+    assert_tag :tag => "a", :content => "Remove", :attributes => {:href => /\/#{@allow_person.id}/}
 
-  test 'have not record actions' do
-    @no_permission.update_attributes(:age => 200)
-    get :index
-    assert_no_tag :tag => "a", :content => "View"
-    assert_no_tag :tag => "a", :content => "Edit"
-    assert_no_tag :tag => "a", :content => "Remove"
+    assert_no_tag :tag => "a", :content => "View", :attributes => {:href => /\/#{@not_allow_person.id}/}
+    assert_no_tag :tag => "a", :content => "Edit", :attributes => {:href => /\/#{@not_allow_person.id}\/edit/}
+    assert_no_tag :tag => "a", :content => "Remove", :attributes => {:href => /\/#{@not_allow_person.id}/}
   end
 
   test 'remove new action in set_dry_view_config ' do
-    @no_permission.update_attributes(:age => 200)
     get :index, :user => "no_permission"
     assert_no_tag :tag => "a", :content => "Newpeople"
   end
@@ -42,8 +35,10 @@ class PeopleControllerTest < ActionController::TestCase
   end
 
   test 'create' do
-    post :create
-    assert_redirected_to "http://test.host/people/2"
+    assert_difference("Person.count") do
+      post :create
+      assert_redirected_to "http://test.host/people/#{Person.last.id}"
+    end
   end
 
   test 'no permission create' do
@@ -57,59 +52,62 @@ class PeopleControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'index with human name' do
-    get :index
-    assert_human_name
-  end
-
   test 'new with human name' do
     get :new
     assert_human_name
   end
 
   test 'edit with human name' do
-    get :edit, :id => Person.first.id
+    get :edit, :id => @allow_person.id
     assert_human_name
   end
 
   test 'show with human name' do
-    get :show, :id => Person.first.id
+    get :show, :id => @allow_person.id
     assert_human_name
   end
 
+  test "show success" do
+    get :show, :id => @allow_person.id
+    assert_response :success
+  end
+
+  test "show faidure" do
+    assert_record_not_found do
+      get :show, :id => @not_allow_person.id
+    end
+  end
+
   test "edit success" do
-    get :edit, :id => @no_permission.id
+    get :edit, :id => @allow_person.id
     assert_response :success
   end
 
   test "edit faidure" do
-    @no_permission.update_attributes!(:age => 200)
     assert_record_not_found do
-      get :edit, :id => @no_permission.id
+      get :edit, :id => @not_allow_person.id
     end
   end
 
   test "update success" do
-    put :update, :id => @no_permission.id
+    put :update, :id => @allow_person.id
     assert_response :redirect
   end
 
   test "update faidure" do
-    @no_permission.update_attributes!(:age => 200)
     assert_security_error do
-      put :update, :id => @no_permission.id
+      put :update, :id => @not_allow_person.id
     end
   end
 
   test "destroy success" do
-    delete :destroy, :id => @no_permission.id
+    delete :destroy, :id => @allow_person.id
     assert_response :redirect
   end
 
   test "destroy faidure" do
-    @no_permission.update_attributes!(:age => 200)
     assert_security_error do
-      delete :destroy, :id => @no_permission.id
+      delete :destroy, :id => @not_allow_person.id
     end
   end
 
