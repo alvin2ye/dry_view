48433ca59fc5c0d61ed2af689a87f3ad9ab43905
JimJin <jim.jin2006@gmail.com>
2009-09-10 11:22:09 +0800

    fix allow operate control in controller.

--- lib/dry_view.rb
+++ lib/dry_view.rb
@@ -19,6 +19,7 @@ module DryView
 
       unless options[:except_show]
         define_method("show") do
+          raise ActiveRecord::RecordNotFound if resource.respond_to?('allow_show?') && !resource.allow_show?
           show! do |format|
             format.html { render :template => "/dry_view_default/show" if !template_exists?  }
           end
@@ -26,6 +27,7 @@ module DryView
       end
 
       define_method("edit") do
+        raise ActiveRecord::RecordNotFound if resource.respond_to?('allow_edit?') && !resource.allow_edit?
         edit! do |format|
           format.html { render :template => "/dry_view_default/edit" if !template_exists?  }
         end
@@ -38,6 +40,7 @@ module DryView
       end
 
       define_method("update") do
+        raise SecurityError if resource.respond_to?('allow_edit?') && !resource.allow_edit?
         update! do |success, failure|
           failure.html { render :template => template_exists? ? "edit" : "/dry_view_default/edit" }
           success.html { redirect_to options[:except_show] ?  collection_url : resource_url }
@@ -48,6 +51,13 @@ module DryView
         create! do |success, failure|
           failure.html { render :template => template_exists? ? "new" : "/dry_view_default/new" }
           success.html { redirect_to options[:except_show] ?  collection_url : resource_url }
+        end
+      end
+
+      define_method("destroy") do
+        raise SecurityError if resource.respond_to?('allow_destroy?') && !resource.allow_destroy?
+        destroy! do |format|
+          format.html{ redirect_to collection_url}
         end
       end
 
